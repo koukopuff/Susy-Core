056ef6f508afb7046cc8e7794a2d7f26f5cb80cb
koukobegirlish <koukoisferalgirl@gmail.com>
2025-12-18 18:51:28 -0800

    add large compressor multiblock

---
+++ src/main/java/supersymmetry/common/metatileentities/multi/steam/MetaTileEntitySuSyLargeCompressor.java
@@ -0,0 +1,109 @@
+package supersymmetry.common.metatileentities.multi.steam;
+
+import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.client.particle.VanillaParticleEffects;
+import gregtech.client.renderer.texture.Textures;
+import gregtech.client.utils.TooltipHelper;
+import gregtech.common.blocks.BlockGlassCasing;
+import gregtech.common.blocks.BlockMetalCasing;
+import net.minecraft.client.resources.I18n;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import java.util.List;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+import gregtech.api.capability.impl.SteamMultiWorkable;
+import gregtech.api.metatileentity.MetaTileEntity;
+import gregtech.api.metatileentity.multiblock.RecipeMapSteamMultiblockController;
+import gregtech.api.recipes.RecipeMaps;
+import gregtech.api.pattern.BlockPattern;
+import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
+import gregtech.api.metatileentity.multiblock.IMultiblockPart;
+import gregtech.client.renderer.ICubeRenderer;
+import gregtech.common.blocks.MetaBlocks;
+import gregtech.common.ConfigHolder;
+import gregtech.api.unification.material.Materials;
+import supersymmetry.client.renderer.textures.SusyTextures;
+import gregtech.client.renderer.texture.cube.OrientedOverlayRenderer;
+
+
+public class MetaTileEntitySuSyLargeCompressor extends RecipeMapSteamMultiblockController {
+
+    private static final int PARALLEL_LIMIT = 8;
+
+    public MetaTileEntitySuSyLargeCompressor(ResourceLocation metaTileEntityId) {
+        super(metaTileEntityId, RecipeMaps.COMPRESSOR_RECIPES, CONVERSION_RATE);
+        this.recipeMapWorkable = new SteamMultiWorkable(this, CONVERSION_RATE);
+        this.recipeMapWorkable.setParallelLimit(PARALLEL_LIMIT);
+    }
+
+    @Override
+    public MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
+        return new MetaTileEntitySuSyLargeCompressor(this.metaTileEntityId);
+    }
+
+    @Override
+    protected BlockPattern createStructurePattern() {
+        return FactoryBlockPattern.start()
+                .aisle("XXX", "XXX", "XXX", "XXX")
+                .aisle("XXX", "XSX", "XSX", "XXX")
+                .aisle("XMX", "XGX", "XGX", "XXX")
+                .where('G', states(MetaBlocks.TRANSPARENT_CASING.getState(BlockGlassCasing.CasingType.TEMPERED_GLASS)))
+                .where('S', states(MetaBlocks.COMPRESSED.get(Materials.Steel).getBlock(Materials.Steel)))
+                .where('M', this.selfPredicate())
+                .where('X', states(getCasingState()).setMinGlobalLimited(28).or(autoAbilities()))
+                .build();
+    }
+
+    protected static IBlockState getCasingState() {
+        return ConfigHolder.machines.steelSteamMultiblocks ?
+                MetaBlocks.METAL_CASING.getState(BlockMetalCasing.MetalCasingType.STEEL_SOLID) :
+                MetaBlocks.METAL_CASING.getState(BlockMetalCasing.MetalCasingType.BRONZE_BRICKS);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public ICubeRenderer getBaseTexture(IMultiblockPart sourcePart) {
+        return ConfigHolder.machines.steelSteamMultiblocks ?
+                Textures.SOLID_STEEL_CASING : Textures.BRONZE_PLATED_BRICKS;
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World player, @NotNull List<String> tooltip, boolean advanced) {
+        super.addInformation(stack, player, tooltip, advanced);
+        tooltip.add(I18n.format("gregtech.multiblock.steam_.duration_modifier"));
+        tooltip.add(I18n.format("gregtech.universal.tooltip.parallel", PARALLEL_LIMIT));
+        tooltip.add(TooltipHelper.BLINKING_ORANGE + I18n.format("gregtech.multiblock.require_steam_parts"));
+    }
+
+    @Override
+    public boolean hasMaintenanceMechanics() {
+        return false;
+    }
+
+    @Override
+    public int getItemOutputLimit() {
+        return 1;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @NotNull
+    @Override
+    protected ICubeRenderer getFrontOverlay() {
+        return Textures.COMPRESSOR_OVERLAY;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void randomDisplayTick() {
+        if (isActive()) {
+            VanillaParticleEffects.RANDOM_SPARKS.runEffect(this);
+        }
+    }
+}
